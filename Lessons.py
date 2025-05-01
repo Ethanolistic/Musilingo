072348e75f856ea549f4ea6342ef1d1be28de656
Ethanolistic <awesomeethan718@gmail.com>
2025-05-01 10:36:55 -0400

    Musilingo progrss 1

---
+++ Lessons.py
@@ -0,0 +1,16 @@
+# Making classes for each lesson
+def beginner():
+    intro_prompt = input("Welcome to the Beginner lesson!\n Here you will learn about The Layout of the Piano, Enharmonics, and Finger Numbering.\n Ready to continue?")
+    print(intro_prompt)
+    if intro_prompt == "Yes":
+        print("Great! Lets continue!")
+        
+    elif intro_prompt == "No":
+        print("Okay no problem.")
+    else:
+        print("Please type Yes or No!")
+def intermedaite():
+
+def novice():
+
+def advanced():
