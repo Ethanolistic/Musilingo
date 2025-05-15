9e2288cdb903f8fcda701b8ebd93f35c59badb25
Ethanolistic <awesomeethan718@gmail.com>
2025-05-15 13:02:14 -0400

    Musilingo progrss 3

--- Lessons.py
+++ Lessons.py
@@ -14,31 +14,37 @@ def welcome():
 def beginner(lesson_input):
     beginner_quest = True
     beginner1 = True
+    # start beginner lesson
     if lesson_input.lower() == "beginner":
         beginner1 = False
         while beginner1 is False:
             print(
                 "Beginner 1:\n Ok let's begin!\n Let's start with the layout of a piano. If you were to look up an image,\n you'd notice that there are a mixture of black and white keys. The black keys are known as sharps or flats, and the\n white keys are known as naturals. Sharps can be notated using the [♯] symbol,\n Flats can be notated using the [♭] symbol, and naturals can be notated using the [♮] symbol.")
+            # takes in user input to proceed
             Continue_input = input("Ready to move on?")
+            # should print beginner 2 if input is "yes" and should print beginner 1 again if input is "no"
             if Continue_input.lower() == "yes":
                 print("Ok, let's move on!")
                 beginner1 = True
             else:
                 print("Take as much time as you need.")
                 beginner1 = True
+            # print beginner 2
             if Continue_input.lower() == "yes":
                 print("Beginner 2:\n Now that we have some fundamentals,"
                       "lets take a brief look at a concept\n called Enharmonics.\n"
-                      "Enharmonics are essentially the diea that notes on a piano"
+                      "Enharmonics are essentially the idea that notes on a piano"
                       "can have diferent\n names. For example, another name for [C] could be B♯ or D𝄫"
-                      "(double flat). Double flats look advanced but they aren't. So, for the sake of ocntinuity,"
+                      "(double flat). Double flats look advanced but they aren't. So, for the sake of conntinuity,"
                       "we'll save them for later.")
-                Continue_input = input("Ready to move on?")
-                if Continue_input.lower() == "yes":
-                    print("Ok lets go!!")
-                else:
-                    print("No worries. You got all the time in the world.")
-                    beginner1 = True
+            # if input is yes, should print review portion. If no, should print beginner 2 again.
+            Continue_input = input("Ready to move on?")
+            if Continue_input.lower() == "yes":
+                print("Ok lets go!!")
+            else:
+                print("No worries. You got all the time in the world.")
+                beginner1 = True
+            # review portion
             if Continue_input.lower() == "yes":
                 print("Since this is the beginning and you should be new, let's do some review!\n \n"
                       "As we've covered, there are three types of notes on a piano: Sharps[♯],\n Flats[♭], and Naturals[♮]."
@@ -48,40 +54,49 @@ def beginner(lesson_input):
                       "and naturals, you should know how to identify notes on a piano. Lets say that you play an E♭. Another way "
                       "to identify that note is D♯. For naturals, if D♯ is in the key of the piece that is\n being analyzed, and"
                       "a D natural accidental shows up, it should look like this: [D♮]")
-            Continue_input = ("Are you ready for the quiz?")
-            if Continue_input.lower() == "yes":
-                print("Okay! Good luck!")
-                beginner1 = True
-            else:
-                print("Take as much time needed to review!")
+                # prompt user to start the quiz.
+                Continue_input = ("Are you ready for the quiz?")
+                # if yes, go on to beginner_quest. If no, keep user there and reprint "Ready to move on?"
+                if Continue_input.lower() == "yes":
+                    print("Okay! Good luck!")
+                    beginner1 = True
+                else:
+                    print("Take as much time needed to review!")
         while beginner_quest is False:
             print("Okay lets get started!")
+            # question 1
             quest1 = input("What is another name for [C♯]? \n A. D flat\n B. C natural\n C. B double flat")
             if quest1.lower() == "a":
                 print("Good job!")
             elif quest1.lower() == "b" or "c":
                 print("Sorry. Thats not the correct answer was A")
+            # incase user enters jibberish
             else:
                 print("Please enter a valid option")
+            # question 2
             quest2 = input("What is another name for [A♯]?\n A. G flat\n B. B flat\n C. F sharp")
             if quest2.lower() == "b":
                 print("Awesomesauce!")
             elif quest2.lower() == "a" or "c":
                 print("Aw man! The correct answer was B")
+            # incase user enters jibberish
             else:
                 print("Please enter a valid option")
+            # question 3
             quest3 = input("What is another name for [F♭]?\n A. F sharp\n B. E flat\n C. E natural")
             if quest3.lower() == "c":
                 print("Amazing!")
             elif quest3.lower() == "a" or "b":
                 print("C'mon dude. The right answer was C :(")
+            # incase user enters jibberish
             else:
                 print("Please enter a letter!")
                 beginner_quest = True
 
-        home_page = input("Press H to return home!")
-        if home_page.lower() == "h":
-            return welcome()
+            # prompt user to return back to lesson choices.
+            home_page = input("Press H to return home!")
+            if home_page.lower() == "h":
+                return welcome()
 
 
 #def intermedaite():
